7a44b2ada12296221ba06c31f3dc16a66305acec
Cameron Taylor <TheJimmyBlaze@gmail.com>
2024-02-14 10:48:17 +1300

    Add bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@
 const App = () => {
 
   return (
-    <h1>Web Things</h1>
+    <div className="vh-100 d-flex flex-column justify-content-center align-items-center bg-dark text-light overflow-hidden">
+      <h1>Web Things</h1>
+    </div>
   )
 };
 
