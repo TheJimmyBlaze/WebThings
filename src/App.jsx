c98f5aa1cf86b018fead50b14dcd082357180d30
Cameron Taylor <TheJimmyBlaze@gmail.com>
2024-02-14 14:42:46 +1300

    Add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Container } from 'react-bootstrap';
+
+import Loader from './features/loader/Loader';
 
 const App = () => {
 
   return (
     <div className="vh-100 d-flex flex-column justify-content-center align-items-center bg-dark text-light overflow-hidden">
-      <h1>Web Things</h1>
+
+      <Container>
+
+        <Loader />
+      </Container>
     </div>
   )
 };
---
+++ src/features/widgets/container/Container.jsx
@@ -0,0 +1,6 @@
+
+const Container =  () => {
+
+};
+
+export default Container;
